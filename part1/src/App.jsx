2dd495ba8f66eca610c71ee72f255ce0ba309532
ehye <yjw9610@hotmail.com>
2023-10-24 18:22:48 +0800

    1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,8 +2,18 @@ const Header = (props) => {
   return (<h1>{props.course}</h1>)
 }
 
+const Part = (props) => {
+  return (<p>{props.part} {props.exercise}</p>)
+}
+
 const Content = (props) => {
-  return (<p>{props.part} {props.exercises}</p>)
+  return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </div>
+  )
 }
 
 const Total = (props) => {
@@ -22,9 +32,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total amount={exercises1 + exercises2 + exercises3} />
     </div>
   )
