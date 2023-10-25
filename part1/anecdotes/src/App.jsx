6f4a2147074f0a8fe089dc2e73825d9f40c6a6a3
ehye <yjw9610@hotmail.com>
2023-10-25 18:19:13 +0800

    1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
 
   const [index, setIndex] = useState(0)
   const [votes, setVote] = useState(new Uint8Array(anecdotes.length))
+  const [most, setMost] = useState(0)
 
   const nextAnecdote = () => {
     setIndex(Math.floor(Math.random() * (anecdotes.length)))
@@ -23,16 +24,22 @@ const App = () => {
     const copy = { ...votes }
     copy[index] += 1
     setVote(copy)
-    // console.log(index);
-    // console.log(copy);
+    console.log("index:", index, "votes[index]:", votes[index], "most:", most, "votes[most]:", votes[most]);
+    if (votes[index] >= votes[most]) {
+      setMost(index);
+    }
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <div>{anecdotes[index]}</div>
       <div>has {votes[index]} votes</div>
       <button onClick={addVote}>vote</button>
       <button onClick={nextAnecdote}>next anecdotes</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[most]}</div>
+      <div>has {votes[most]} votes</div>
     </div>
   )
 }
