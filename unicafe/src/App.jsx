c1fb471d0c8c9caf67eadcfe4ea09cc1ddfddebb
ehye <yjw9610@hotmail.com>
2023-10-25 11:24:49 +0800

    1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -7,32 +7,39 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const increaseByone = () => {
+  const addGood = () => {
     return setGood(good + 1)
   }
-  const decreaseByone = () => {
+  const addNeutral = () => {
     return setNeutral(neutral + 1)
   }
-  const setToZero = () => {
+  const addBad = () => {
     return setBad(bad + 1)
   }
 
-  const Display = ({ text, counter }) => 
+  const Display = ({ text, counter }) =>
     <div>{text} {counter}</div>
+
+  const Display1 = ({ text, counter }) =>
+    <div>{text} {counter} %</div>
 
   const Button = ({ handleClick, text }) =>
     <button onClick={handleClick}>{text}</button>
 
+  const all = good + neutral + bad
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text="good" handleClick={increaseByone} />
-      <Button text="neutral" handleClick={decreaseByone} />
-      <Button text="bad" handleClick={setToZero} />
+      <Button text="good" handleClick={addGood} />
+      <Button text="neutral" handleClick={addNeutral} />
+      <Button text="bad" handleClick={addBad} />
       <h1>statistics</h1>
       <Display text="good" counter={good} />
       <Display text="neutral" counter={neutral} />
       <Display text="bad" counter={bad} />
+      <Display text="all" counter={all} />
+      <Display text="average" counter={(good - bad) / all} />
+      <Display1 text="positive" counter={100 * good / all} />
     </div>
   )
 }
