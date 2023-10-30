8c172d5d87766c954a2a40c1887fc92f01f6f389
ehye <yjw9610@hotmail.com>
2023-10-30 10:01:31 +0800

    2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import personService from './services/person'
-import person from './services/person'
+import './index.css'
 
 const Filter = ({ filter, onFilterChange }) => (
   <div>
@@ -8,60 +8,6 @@ const Filter = ({ filter, onFilterChange }) => (
     <input value={filter} onChange={onFilterChange} />
   </div>
 )
-
-const PersonForm = ({
-  persons,
-  newName,
-  handleNameChange,
-  newNumber,
-  handleNumberChange,
-  setPersons,
-  setNewName,
-  setNewNumber,
-}) => {
-  const addPerson = (event) => {
-    event.preventDefault()
-    const matchedPersonsArray = persons.filter((p) => p.name === newName)
-    if (matchedPersonsArray.length > 0) {
-      if (
-        window.confirm(
-          `${newName} is already added to phonebook, replace the old number with a new one?`
-        )
-      ) {
-        const id = matchedPersonsArray[0].id
-        const personObject = {
-          id: id,
-          name: newName,
-          number: newNumber,
-        }
-        personService.update(id, personObject)
-      }
-    } else {
-      const personObject = {
-        name: newName,
-        number: newNumber,
-      }
-      personService.create(personObject)
-      setPersons(persons.concat(personObject))
-    }
-
-    setNewName('')
-    setNewNumber('')
-  }
-  return (
-    <form onSubmit={addPerson}>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-      </div>
-      <div>
-        number: <input value={newNumber} onChange={handleNumberChange} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  )
-}
 
 const Persons = ({ persons, filter }) => {
   const deletePerson = (event) => {
@@ -84,17 +30,75 @@ const Persons = ({ persons, filter }) => {
     ))
 }
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return <h1>{message}</h1>
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setSearch] = useState('')
+  const [addedMessage, setAddedMessage] = useState('some error happened...')
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
       setPersons(initialPersons)
     })
   }, [])
+
+  const PersonForm = () => {
+    const addPerson = (event) => {
+      event.preventDefault()
+      const matchedPersonsArray = persons.filter((p) => p.name === newName)
+      if (matchedPersonsArray.length > 0) {
+        if (
+          window.confirm(
+            `${newName} is already added to phonebook, replace the old number with a new one?`
+          )
+        ) {
+          const id = matchedPersonsArray[0].id
+          const personObject = {
+            id: id,
+            name: newName,
+            number: newNumber,
+          }
+          personService.update(id, personObject)
+        }
+      } else {
+        const personObject = {
+          name: newName,
+          number: newNumber,
+        }
+        personService.create(personObject)
+        setPersons(persons.concat(personObject))
+        setAddedMessage(`Added '${newName}'`)
+        setTimeout(() => {
+          setAddedMessage(null)
+        }, 5000)
+      }
+
+      setNewName('')
+      setNewNumber('')
+    }
+    return (
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    )
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -112,18 +116,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={addedMessage} />
       <Filter filter={filter} onFilterChange={handleFilterChange} />
       <h3>add a new</h3>
-      <PersonForm
-        persons={persons}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-        setNewName={setNewName}
-        setNewNumber={setNewNumber}
-        setPersons={setPersons}
-      />
+      <PersonForm />
       <h3>Numbers</h3>
       <Persons persons={persons} filter={filter} />
     </div>
