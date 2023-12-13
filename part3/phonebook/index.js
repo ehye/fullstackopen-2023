9792021835aef56284c1239ca58f0e965871323f
ehye <yjw9610@hotmail.com>
2023-12-14 00:33:47 +0800

    3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,28 +25,16 @@ app.use(morgan(function (tokens, req, res) {
 
 app.use(express.static('dist'))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
     }
-]
+
+    next(error)
+}
+app.use(errorHandler)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -64,14 +52,16 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-
-    const person = persons.find(p => p.id === Number(request.params.id))
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    PhoneBook.findById(request.params.id)
+        .then(p => {
+            if (p) {
+                response.json(p)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
