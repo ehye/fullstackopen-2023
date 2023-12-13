0b8875a9fb99955b20520e5ffcdc8120ef83b295
ehye <yjw9610@hotmail.com>
2023-12-14 00:14:30 +0800

    3.13

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,45 +1,22 @@
-const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
+// if (newName === null || newNumber == null) {
+    // Person.find({}).then(result => {
+    //     if (result.length > 0) {
+    //         console.log('phonebook:')
+    //         result.forEach(person => {
+    //             console.log(person.name, person.number)
+    //         })
+    //         mongoose.connection.close()
+    //     }
+    // })
+// } else {
+//     const person = new Person({
+//         name: newName,
+//         number: newNumber,
+//     })
 
-const password = process.argv[2]
-const newName = process.argv[3]
-const newNumber = process.argv[4]
-
-const url =
-    `mongodb+srv://fullstack:${password}@cluster0.zxtcifc.mongodb.net/?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
-
-const Person = mongoose.model('phonebook', personSchema)
-
-if (newName === null || newNumber == null) {
-    Person.find({}).then(result => {
-        if (result.length > 0) {
-            console.log('phonebook:')
-            result.forEach(person => {
-                console.log(person.name, person.number)
-            })
-            mongoose.connection.close()
-        }
-    })
-} else {
-    const person = new Person({
-        name: newName,
-        number: newNumber,
-    })
-
-    person.save().then(result => {
-        console.log(`added ${newName} ${newNumber} to phonebook`)
-        mongoose.connection.close()
-    })
-}
+//     person.save().then(result => {
+//         console.log(`added ${newName} ${newNumber} to phonebook`)
+//         mongoose.connection.close()
+//     })
+// }
