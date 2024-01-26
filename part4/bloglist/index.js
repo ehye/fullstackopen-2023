24b55107afe2ab06201cc7a3359639c5efd21e4b
ehye <yjw9610@hotmail.com>
2024-01-26 17:59:01 +0800

    4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,26 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
