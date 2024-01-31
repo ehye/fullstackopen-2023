4f2313190058b025fab67a388ad93f95fdec6cf9
ehye <yjw9610@hotmail.com>
2024-01-31 11:04:11 +0800

    4.8

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
