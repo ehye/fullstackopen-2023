a6a4880be7ce90afee2848f4aa39a8b7726f1a7a
ehye <yjw9610@hotmail.com>
2024-02-04 18:53:10 +0800

    4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,6 +8,7 @@ require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
