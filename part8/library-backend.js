f362039f7bbd2b93e2d1a5684371e674870e15b5
ehye <yjw9610@hotmail.com>
2024-03-04 23:05:57 +0800

    8.3

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -95,10 +95,16 @@ const typeDefs = `
     genres: [String]
   }
 
+  type Author {
+    name: String
+    bookCount: Int
+  }
+
   type Query {
     bookCount: Int,
     authorCount: Int,
-    allBooks: [Book!]!
+    allBooks: [Book!]!,
+    allAuthors: [Author!]!
   }
 `
 
@@ -107,6 +113,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () =>
+      authors.map(author => ({
+        name: author.name,
+        bookCount: books.filter(book => book.author == author.name).length,
+      })),
   },
 }
 
