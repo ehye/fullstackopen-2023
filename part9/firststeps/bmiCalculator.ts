3c30a08dddb83563481bdf69b8837340ec263166
ehye <yjw9610@hotmail.com>
2024-03-07 22:06:15 +0800

    9.3

--- part9/firststeps/bmiCalculator.ts
+++ part9/firststeps/bmiCalculator.ts
@@ -1,3 +1,24 @@
+import { isNotNumber } from './utils'
+
+interface BmiValues {
+  height: number
+  mass: number
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNotNumber(args[2]) && !isNotNumber(args[3])) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
 const calculateBmi = (height: number, mass: number): string => {
   const bmi = mass / Math.pow(height / 100, 2)
   let result: string
@@ -23,4 +44,5 @@ const calculateBmi = (height: number, mass: number): string => {
   return result
 }
 
-console.log(calculateBmi(180, 74))
+const { height, mass } = parseArguments(process.argv)
+console.log(calculateBmi(height, mass))
--- part9/firststeps/exerciseCalculator.ts
+++ part9/firststeps/exerciseCalculator.ts
@@ -1,3 +1,29 @@
+import { isNotNumber } from './utils'
+
+interface MultipleValues {
+  dailyExerciseHours: Array<number>
+  targetAmount: number
+}
+
+const parseArguments = (args: string[]): MultipleValues => {
+  if (args.length < 12) throw new Error('Not enough arguments')
+  if (args.length > 12) throw new Error('Too many arguments')
+
+  let dailyExerciseHours: Array<number> = []
+  for (let index = 3; index < args.length; index++) {
+    if (!isNotNumber(args[index])) {
+      dailyExerciseHours.push(Number(args[index]))
+    } else {
+      throw new Error('Provided values were not numbers!')
+    }
+  }
+
+  return {
+    dailyExerciseHours,
+    targetAmount: Number(args[2]),
+  }
+}
+
 interface Result {
   periodLength: number
   trainingDays: number
@@ -9,8 +35,10 @@ interface Result {
 }
 
 const calculateExercises = (dailyExerciseHours: Array<number>, targetAmount: number): Result => {
+  console.log(dailyExerciseHours, 'length:', dailyExerciseHours.length)
+
   const trainingDays = dailyExerciseHours.filter(x => x > 0).length
-  const average = dailyExerciseHours.reduce((a, b) => a + b) / 7
+  const average = dailyExerciseHours.reduce((a, b) => a + b, 0) / dailyExerciseHours.length
   const ratingDescription = [
     'try harder',
     'congratulation you have finish the training ',
@@ -31,4 +59,6 @@ const calculateExercises = (dailyExerciseHours: Array<number>, targetAmount: num
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const { dailyExerciseHours, targetAmount } = parseArguments(process.argv)
+
+console.log(calculateExercises(dailyExerciseHours, targetAmount))
