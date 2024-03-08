cb372dbdf6ba70ec86e24142a8e1620d225c26c8
ehye <yjw9610@hotmail.com>
2024-03-08 16:03:50 +0800

    9.5

--- part9/firststeps/bmiCalculator.ts
+++ part9/firststeps/bmiCalculator.ts
@@ -5,44 +5,59 @@ interface BmiValues {
   mass: number
 }
 
-const parseArguments = (args: string[]): BmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments')
-  if (args.length > 4) throw new Error('Too many arguments')
+interface BmiResult {
+  weight: number
+  height: number
+  bmi: string
+}
 
-  if (!isNotNumber(args[2]) && !isNotNumber(args[3])) {
+const parseArguments = (args: any): BmiValues | { error: string } => {
+  if (args.length < 2) throw new Error('Not enough arguments')
+  if (args.length > 2) throw new Error('Too many arguments')
+
+  if (!isNotNumber(args.height) && !isNotNumber(args.weight)) {
     return {
-      height: Number(args[2]),
-      mass: Number(args[3]),
+      height: Number(args.height),
+      mass: Number(args.weight),
     }
   } else {
-    throw new Error('Provided values were not numbers!')
+    return {
+      error: 'malformatted parameters',
+    }
   }
 }
 
-const calculateBmi = (height: number, mass: number): string => {
-  const bmi = mass / Math.pow(height / 100, 2)
-  let result: string
+export const calculateBmi = (args: any): BmiResult | { error: string } => {
+  const arg = parseArguments(args)
+  let result
+  if ('height' in arg) {
+    const bmi = arg.mass / Math.pow(arg.height / 100, 2)
+    result = {
+      weight: arg.mass,
+      height: arg.height,
+      bmi: '',
+    }
 
-  if (bmi < 16.0) {
-    result = 'Underweight (Severe thinness)'
-  } else if (16.0 <= bmi && bmi <= 16.9) {
-    result = 'Underweight (Moderate thinness)'
-  } else if (17 <= bmi && bmi <= 18.4) {
-    result = 'Underweight (Mild thinness)'
-  } else if (18.5 <= bmi && bmi <= 24.9) {
-    result = 'Normal (healthy weight)'
-  } else if (25 <= bmi && bmi <= 29.9) {
-    result = 'Overweight'
-  } else if (30.0 <= bmi && bmi <= 34.9) {
-    result = 'Obesity (Class I) '
-  } else if (35.0 <= bmi && bmi <= 39.9) {
-    result = 'Obesity (Class II) '
-  } else if (bmi >= 40) {
-    result = 'Obesity (Class III) '
+    if (bmi < 16.0) {
+      result.bmi = 'Underweight (Severe thinness)'
+    } else if (16.0 <= bmi && bmi <= 16.9) {
+      result.bmi = 'Underweight (Moderate thinness)'
+    } else if (17 <= bmi && bmi <= 18.4) {
+      result.bmi = 'Underweight (Mild thinness)'
+    } else if (18.5 <= bmi && bmi <= 24.9) {
+      result.bmi = 'Normal (healthy weight)'
+    } else if (25 <= bmi && bmi <= 29.9) {
+      result.bmi = 'Overweight'
+    } else if (30.0 <= bmi && bmi <= 34.9) {
+      result.bmi = 'Obesity (Class I) '
+    } else if (35.0 <= bmi && bmi <= 39.9) {
+      result.bmi = 'Obesity (Class II) '
+    } else if (bmi >= 40) {
+      result.bmi = 'Obesity (Class III) '
+    }
+  } else {
+    result = arg
   }
 
   return result
 }
-
-const { height, mass } = parseArguments(process.argv)
-console.log(calculateBmi(height, mass))
--- part9/firststeps/index.ts
+++ part9/firststeps/index.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import { calculateBmi } from './bmiCalculator'
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack')
+})
+
+app.get('/bmi', (req, res) => {
+  const bmi = calculateBmi(req.query)
+  res.send(bmi)
 })
 
 const PORT = 3003
